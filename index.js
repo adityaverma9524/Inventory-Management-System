78ddaf295733730ebbd522b2ae3fc4b9a8edfbfb
Aditya Verma <adityaverma9524@gmail.com>
2024-07-04 11:37:38 +0530

    commit

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import ProductsController from './src/controllers/product.controller.js';
+import UserController from './src/controllers/user.controller.js';
 import ejsLayouts from 'express-ejs-layouts';
 import path from 'path';
 import validationMiddleware from './src/middlewares/validation.middleware.js';
 import { uploadFile } from './src/middlewares/file-upload.middleware.js';
-
+import session from 'express-session';
+import {auth} from './src/middlewares/auth.middleware.js'
 const app = express();
 
 app.use(express.static('public'));
+app.use(session({
+  secret:'SecretKey',
+  resave: false,
+  saveUninitialized: true,
+  cookie:{secure:false},
+}));
 
 const productsController =
   new ProductsController();
+const usersController = new UserController();
 
 app.use(ejsLayouts);
 app.use(express.json());
@@ -21,31 +30,39 @@ app.set(
   path.join(path.resolve(), 'src', 'views')
 );
 
-app.get('/', productsController.getProducts);
+app.get('/register', usersController.getRegister);
+app.get('/login', usersController.getLogin);
+app.post('/login', usersController.postLogin);
+app.get('/logout', usersController.logout);
+app.post(
+  '/register',
+  usersController.postRegister
+);
+app.get('/',auth, productsController.getProducts);
 app.get(
-  '/add-product',
+  '/add-product',auth,
   productsController.getAddProduct
 );
 
 app.get(
-  '/update-product/:id',
+  '/update-product/:id',auth,
   productsController.getUpdateProductView
 );
 
 app.post(
-  '/delete-product/:id',
+  '/delete-product/:id',auth,
   productsController.deleteProduct
 );
 
 app.post(
-  '/',
+  '/',auth,
   uploadFile.single('imageUrl'),
   validationMiddleware,
   productsController.postAddProduct
 );
 
 app.post(
-  '/update-product',
+  '/update-product',auth,
   productsController.postUpdateProduct
 );
 
